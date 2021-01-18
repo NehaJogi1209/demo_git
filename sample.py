18288ea8011b1113e26924d03c2e218a181bd5db
NehaJogi1209 <nehajogi12@gmail.com>
2021-01-18 13:37:58 +0530

    "This is mu first commit from pycharm"

--- sample.py
+++ sample.py
@@ -7,3 +7,6 @@ print("This is my first update from pycharm")
 print("The changes done by github..")
 
 print("This is my second update from pycharm")
+
+print("This is my third update from pycharm")
+
